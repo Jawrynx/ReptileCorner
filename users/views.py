b6a65d05d3d090719bd6717c57477f95d74284ae
Jawrynx <jawrynx@gmail.com>
2024-10-03 13:49:54 +0100

    Added User Registration / Sign Up area to Home page

--- users/views.py
+++ users/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def register_view(request):
-    return HttpResponse('User Registration Page')
+    form = UserCreationForm()
+    return render(request, 'users/register.html', { 'form': form })
