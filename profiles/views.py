8b4c549a418e779c87d1c4aa2fb6088e8fad94a8
Jawrynx <jawrynx@gmail.com>
2024-10-14 10:40:57 +0100

    Added User posts to their user profiles. Adjusted so I there is 4 or less posts, to centre the posts on desktop screens

--- profiles/views.py
+++ profiles/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import ProfileUpdateForm
 from django.contrib import messages
+
 from django.contrib.auth.models import User
+from posts.models import Post
 
 @login_required
 def profile(request, username): 
-    user = get_object_or_404(User, username=username) 
+    user = get_object_or_404(User, username=username)
+    posts = Post.objects.filter(author=user).order_by('-date') 
     context = {
-        'user': user 
+        'user': user, 
+        'posts': posts
     }
     return render(request, 'profiles/profile.html', context)
 
