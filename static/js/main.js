9b45921eb20c151192ea720a6858c05496800169
Jawrynx <jawrynx@gmail.com>
2024-10-14 10:50:03 +0100

    Changed JS so instead of changing the display to flex and centreing for 4 or less posts, it sets the grid from col-xl-4 to col-xl-3!

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const postsList = document.querySelector('.posts-list');
     const posts = postsList.querySelectorAll('.post');
-    /**Desktop Screensizes. Will adjust and centre posts if there are 4 or less posts avaialable */
-    function centerPosts() {
+  
+    function adjustPostLayout() {
       if (posts.length <= 4 && window.innerWidth >= 1280) {
-        postsList.style.setProperty('display', 'flex', 'important');
-        postsList.style.justifyContent = 'center';
-        console.log("Display style:", postsList.style.display);
+        posts.forEach(post => {
+          post.classList.remove('col-xl-3');
+          post.classList.add('col-xl-4');
+        });
       } else {
-        // Reset the styles for smaller screens
-        postsList.style.display = '';
-        postsList.style.justifyContent = '';
+        posts.forEach(post => {
+          post.classList.remove('col-xl-4');
+          post.classList.add('col-xl-3');
+        });
       }
     }
-
-    centerPosts(); 
-    window.addEventListener('resize', centerPosts); 
+  
+    adjustPostLayout();
+    window.addEventListener('resize', adjustPostLayout);
 });
