149d5491498f99b73ad83a292797e5344d41f634
Jawrynx <jawrynx@gmail.com>
2024-11-20 14:12:28 +0000

    Created Reptiles App to allow users to add their own reptile with only basic views at the moment

---
+++ reptiles/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Reptile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reptiles')
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    bio = models.TextField()
+    reptile_picture = models.ImageField(upload_to='reptile_pics/', blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Reptile'
