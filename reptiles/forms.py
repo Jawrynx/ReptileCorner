3a886da1d216b24124fc53c4a4e17e577c07f591
Jawrynx <jawrynx@gmail.com>
2024-11-20 17:13:21 +0000

    Added 'species' to Reptiles model

--- reptiles/forms.py
+++ reptiles/forms.py
@@ -4,4 +4,4 @@ from .models import Reptile
 class ReptileForm(forms.ModelForm):
     class Meta:
         model = Reptile
-        fields = ['name', 'age', 'bio', 'reptile_picture']
+        fields = ['name', 'age', 'species', 'bio', 'reptile_picture']
